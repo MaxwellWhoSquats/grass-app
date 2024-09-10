cb850fdc9c4a8149c2d9edcece442e3f45be5fcf
MAX ELEY <maxeley2@gmail.com>
2024-09-10 18:26:53 -0400

    Build customer api

---
+++ app/api/customers/route.ts
@@ -0,0 +1,30 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import prisma from "@/prisma/db";
+
+// Define the schema
+const createCustomerSchema = z.object({
+    name: z.string().min(1).max(50),
+    email: z.string().min(1).max(255),
+    phone: z.string().min(1).max(14),
+  });
+
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+
+    // Validate the request
+    const validation = createCustomerSchema.safeParse(body);
+    if (!validation.success)
+        return NextResponse.json({ error: validation.error.errors }, { status: 400 });
+
+    // Save the inventory item to the database
+    const newCustomerItem = await prisma.customer.create({
+        data: {
+            name: body.name,
+            email: body.email,
+            phone: body.phone,
+        }
+    });
+
+    return NextResponse.json(newCustomerItem, { status: 201 });
+}
